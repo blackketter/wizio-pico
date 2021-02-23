ae102a0071bd00ff2a5df8ac91172ae7a1924fbf
Georgi Angelov <the.wizarda@gmail.com>
2021-02-23 16:06:21 +0200

    Add files via upload

---
+++ examples/arduino/pico-pwm/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <PWM.h>
+
+PWMClass PWM(LED);
+
+int duty = 0, direction = 1;
+
+void setup()
+{
+  Serial.begin(115200, true);
+  Serial.println("\nArdiuno Raspberrypi PI Pico 2021 Georgi Angelov");
+  //PWM.setFreq(2000); // other freq
+}
+
+void loop()
+{
+  duty += direction;
+  if (duty > 255)
+  {
+    duty = 255;
+    direction = -1;
+  }
+  if (duty < 0)
+  {
+    duty = 0;
+    direction = 1;
+  }
+  PWM.setDuty(duty * duty);
+  delay(2);
+}
